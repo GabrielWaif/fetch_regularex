39d1bb180de40f1835ef5c47b08d8b33b040b1f7
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2022-08-26 16:40:51 -0300

    Added form and its Regular Expressions for validation

---
+++ js/form.js
@@ -0,0 +1,49 @@
+const cadastro = document.querySelector('#cadastro');
+const nome = document.querySelector('#nome');
+const tag = document.querySelector('#tag');
+const numero = document.querySelector('#numero');
+const endereco = document.querySelector('#endereco');
+
+nome.addEventListener("blur", () => {
+    const re = /^\S[A-Za-z ]+$/;
+    if(re.test(nome.value)){
+        nome.classList = '' ;
+    }
+    else{
+        nome.classList = 'invalid';
+    }
+});
+
+tag.addEventListener("blur", () => {
+    const re = /^[A-Za-z_.]+$/;
+    if(re.test(tag.value)){
+        tag.classList = '' ;
+    }
+    else{
+        tag.classList = 'invalid';
+    }
+});
+
+numero.addEventListener("blur", () => {
+    const re = /^\+?([0-9]){1,3}(\s?)([0-9]\-?\.?){7,9}(\s?x[0-9]{1,5})?$/;
+    if(re.test(numero.value)){
+        numero.classList = '' ;
+    }
+    else{
+        numero.classList = 'invalid';
+    }
+});
+
+endereco.addEventListener("blur", () => {
+    if(endereco.value == ''){
+        endereco.classList = 'invalid';
+    }
+    else{
+        endereco.classList = '' ;
+    }
+});
+
+cadastro.addEventListener('submit', () => {
+    event.preventDefault();
+    
+});
