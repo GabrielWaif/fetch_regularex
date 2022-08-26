6d5652eb120d02b9dd5535552be4b981cc7d36d5
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2022-08-26 13:00:27 -0300

    Added the loop through the fetch, symplified repetitive text with fuction

--- js/app.js
+++ js/app.js
@@ -1,24 +1,38 @@
-const cadastrados = document.querySelector('#cadastrados');
+const cadastrados = document.querySelector("#cadastrados");
 const http = new Http();
 
 http
   .get("https://random-data-api.com/api/v2/users?size=5?response_type=json")
   .then((res) => {
     console.log(res);
+    res.forEach((item) => {
+      const usuario = document.createElement("div");
+      usuario.id = "usuario";
 
-    const item = res[0];
+      const nome = document.createElement("h1");
+      nome.innerHTML = item.first_name;
+      usuario.appendChild(nome);
 
-    const usuario = document.createElement("div");
-    usuario.id = "usuario";
-    const nome = document.createElement("h1");
-    nome.innerHTML = item.first_name;
-    usuario.appendChild(nome);
-    const nomeCompleto = document.createElement("p");
-    nomeCompleto.innerHTML = `Nome: ${item.first_name} ${item.last_name}`;
-    usuario.appendChild(nomeCompleto);
-    const numero = document.createElement("p");
-    numero.innerHTML = `Número: ${item.phone_number}`;
-    usuario.appendChild(numero);
-    cadastrados.appendChild(usuario);
+      const nomeCompleto = createChild(`Nome: ${item.first_name} ${item.last_name}`);
+      usuario.appendChild(nomeCompleto);
+
+      const arroba = createChild(`Tag: @${item.username}`);
+      usuario.appendChild(arroba);
+
+      const numero = createChild(`Número: ${item.phone_number}`);
+      usuario.appendChild(numero);
+
+      const endereco = createChild(
+        `Endereço: ${item.address.country}, ${item.address.state}, ${item.address.city}, ${item.address.street_address}`
+      );
+      usuario.appendChild(endereco);
+
+      cadastrados.appendChild(usuario);
+    });
   });
 
+function createChild(content) {
+  const info = document.createElement("p");
+  info.innerHTML = content;
+  return info;
+}
